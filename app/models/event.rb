77fa2cccd6c6921c475349088a8ea10e9c1a1445
Derek Kastner <dkastner@gmail.com>
2010-03-27 11:55:22 -0400

    added event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,11 @@ class Event < ActiveRecord::Base
 	has_many :event_volunteers
 	has_many :time_blocks
 
+  validates_presence_of :title
+  validates_numericality_of :number_of_hours, :greater_than => 0
+  validates_numericality_of :minimum_volunteers, :only_integer => true,
+                            :greater_than_or_equal_to => 0
+
   def time
     date.strftime("%H:%M:%S")
   end
