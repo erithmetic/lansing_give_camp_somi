1b48abf7b96b14c295dfc80d0d7a2b5fd4c7044f
croland <cbroland@gmail.com>
2010-03-27 12:44:10 -0400

    Working on event form

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ActiveRecord::Base
-	has_many :users, :through => :event_volunteers
+	has_many :users, :through => :event_volunteers, :class_name => 'User'
 	has_many :event_volunteers
 	has_many :time_blocks
+
+	accepts_nested_attributes_for :users, :allow_destroy => true
 
   def time
     date.strftime("%H:%M:%S")
