f880d5f277fc04dc65009fb7014399cfae8c386b
Derek Kastner <dkastner@gmail.com>
2010-03-28 14:25:54 -0400

    no callbacks on meredith"

--- db/migrate/20100327220145_add_meredith.rb
+++ db/migrate/20100327220145_add_meredith.rb
@@ -1,6 +1,6 @@
 class AddMeredith < ActiveRecord::Migration
   def self.up
-    u = User.build(:email => 'meredithda8@aol.com', :password => 'somi', :admin => true, :spamValidation=>"1234")
+    u = User.new(:email => 'meredithda8@aol.com', :password => 'somi', :admin => true, :spamValidation=>"1234")
     u.save(false)
   end
 
