aec1367bce6b31858e9991fb498905179aaa4b8d
Derek Kastner <dkastner@gmail.com>
2010-03-28 09:40:45 -0400

    display of events includes numbers in group. helperized stuff that was in models

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,11 @@ class Event < ActiveRecord::Base
   include ActionView::Helpers::TextHelper
 
 	has_many :users, :through => :event_volunteers, :class_name => 'User'
-	has_many :event_volunteers
+	has_many :event_volunteers do
+    def total
+      self.sum(:number_in_group)
+    end
+  end
 
   named_scope :past, :conditions => ['date <= ?', Time.now]
   named_scope :upcoming, :conditions => ['date > ?', Time.now]
@@ -15,38 +19,13 @@ class Event < ActiveRecord::Base
   default_value_for :maximum_volunteers, 0
   default_value_for :minimum_volunteers, 0
 
-  def timeWithDuration
-    return nil if date.nil?
-    return date if number_of_hours.nil?
-
-  	laterDate = date + number_of_hours.hours
-  	
-  	if number_of_hours <= 1
-  		hourOrHours = "hour"
-  	else
-  		hourOrHours = "hours"
-  	end
-  	
-    date_string = date.strftime("%I:%M %p")
-    later_date_string = laterDate.strftime("%I:%M %p")
-    hours = pluralize(number_of_hours, 'hour')
-    "#{date_string} - #{later_date_string} ( #{number_of_hours} #{hours} )" 
-  end
-
   def past?
     return false if date.nil?
     date <= Time.now
   end
   
-  def volunteersFormatted
-    max_volunteers = maximum_volunteers || 0
-  	num_needed = max_volunteers - users.count
-  	
-  	if full?
-      return users.count
-  	else
-      return "#{users.count} (#{num_needed} more needed)"
-  	end
+  def total_volunteers
+    event_volunteers.total
   end
   
   def full?
