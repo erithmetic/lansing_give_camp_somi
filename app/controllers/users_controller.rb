dde869228395f04c49d5e0d8e0aae4ab86733053
Derek Kastner <dkastner@gmail.com>
2010-03-28 10:39:31 -0400

    removed merge text

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,26 +19,6 @@ class UsersController < ResourceController::Base
 		end
   end
 
-<<<<<<< HEAD
-	# GET /users/1/admin_dashboard
-	# GET /users/1/admin_dashboard.xml
-	def admin_dashboard
-    @user = User.find(params[:id])
-		@upcoming_events = Event.upcoming
-		@past_events = Event.past
-
-		if not @user.admin?
-			redirect_to :action => 'dashboard' 
-		else
-    	respond_to do |format|
-      	format.html # admin_dashboard.html.erb
-      	format.xml  { render :xml => @user }
-    	end
-		end
-	end
-	
-=======
->>>>>>> fixed some formatting and user dashboard
 private
 	def prevent_unauthorized_access
 		forbid if current_user != object and !current_user.admin?
